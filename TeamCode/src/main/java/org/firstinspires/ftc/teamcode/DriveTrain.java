cf26e6d7b6b6527b38ea7c119b50ae407e5ad207
652Underdogs <71893370+652Underdogs@users.noreply.github.com>
2020-10-21 15:43:23 -0400

    Work from 10/21/20 in class - Josh

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTrain.java
@@ -2,28 +2,43 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name = "DriveTrain")
 public class DriveTrain extends LinearOpMode
 {
+    //Drive Motors
     private DcMotor motorFrontLeft;
     private DcMotor motorFrontRight;
     private DcMotor motorBackLeft;
     private DcMotor motorBackRight;
+    //Collection Servos
+    private Servo servo;
+    private CRServo pull;
 
+    //Wobble
+    private DcMotor ClawPivot;
+    private Servo pinch;
     @Override
     public void runOpMode() throws InterruptedException
     {
-        motorFrontLeft = hardwareMap.dcMotor.get("motorFrontLeft");
-        motorFrontRight = hardwareMap.dcMotor.get("motorFrontRight");
-        motorBackLeft = hardwareMap.dcMotor.get("motorBackLeft");
-        motorBackRight = hardwareMap.dcMotor.get("motorBackRight");
+        motorFrontLeft = hardwareMap.dcMotor.get("FrontLeft");
+        motorFrontRight = hardwareMap.dcMotor.get("FrontRight");
+        motorBackLeft = hardwareMap.dcMotor.get("BackLeft");
+        motorBackRight = hardwareMap.dcMotor.get("BackRight");
+
 
         motorBackLeft.setDirection(DcMotor.Direction.REVERSE);
         motorFrontLeft.setDirection(DcMotor.Direction.REVERSE);
 
+        servo = hardwareMap.servo.get("pivot");
+        pull = hardwareMap.crservo.get("pull");
+
+        ClawPivot = hardwareMap.dcMotor.get("ClawPivot");
+        pinch = hardwareMap.servo.get("pinch");
         waitForStart();
 
         while(opModeIsActive())
@@ -33,8 +48,20 @@ public class DriveTrain extends LinearOpMode
             motorFrontRight.setPower(-gamepad1.right_stick_y);
             motorBackRight.setPower(-gamepad1.right_stick_y);
 
+            servo.setPosition(0.8);
+
+            if(gamepad1.a) {
+                pull.setPower(1);
+            } else {
+                pull.setPower(0);
+            }
+
+            ClawPivot.setPower(gamepad1.left_trigger);
+            if(gamepad1.x) {
+                pinch.setPosition(0.5);
+            }
             idle();
         }
-
     }
+
 }
